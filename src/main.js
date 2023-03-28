c1d3ef312aacce08cd2ff1e5db8db4b7c44a4879
Graham Hart <graham@harts.net>
2023-03-27 21:32:22 -0700

    workaround bc canvasjs docs are broken

--- src/main.js
+++ src/main.js
@@ -1,13 +1,30 @@
 const ctx = document.getElementById("mychart")
 
+
+const splitData = (data) => {
+    const xVal = []
+    const yVal = []
+    for (pt of data) {
+        xVal.push(pt.x);
+        yVal.push(pt.y)
+    }
+    return {x: xVal, y:yVal}
+}
+
+const DATA = [{x: 0, y:10}, {x: 1, y: 15}, {x: 3, y: 18}, {x: 4, y: 7}]
+const sD = splitData(DATA);
+
+
 const cfg = {
     type: 'line',
     data: {
-      datasets: [{
-        data: [{x: 10, y: 20}, {x: 15, y: 3}, {x: 20, y: 10}]
-      }]
+        labels: sD.x,
+        datasets: [{
+            data: sD.y,
+        }],
     }
-  }
- 
-  
+}
+
+
+
 new Chart(ctx, cfg);
