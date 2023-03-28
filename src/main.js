4fb82e0f12e0fd026f168afcf2eeadfa10ef59ec
Graham Hart <graham@harts.net>
2023-03-27 22:17:38 -0700

    working graph (i totally wasn't looking at two separate libraries earlier) with sleep & exercise

--- src/main.js
+++ src/main.js
@@ -1,30 +1,62 @@
-const ctx = document.getElementById("mychart")
+// Dummy data
+const sleepData = [{x: 0, y:4}, {x: 1, y: 7}, {x: 3, y: 10}, {x: 4, y: 9.5}] 
+const exerciseData = [{x: 0, y:0}, {x: 1, y: 20}, {x: 3, y: 80}, {x: 4, y: 45}]
 
-
-const splitData = (data) => {
-    const xVal = []
-    const yVal = []
-    for (pt of data) {
-        xVal.push(pt.x);
-        yVal.push(pt.y)
-    }
-    return {x: xVal, y:yVal}
-}
-
-const DATA = [{x: 0, y:10}, {x: 1, y: 15}, {x: 3, y: 18}, {x: 4, y: 7}]
-const sD = splitData(DATA);
-
-
+// Config for graph
 const cfg = {
+    title: {
+        text: "Sleep vs. Exercise Time",
+    },
+    axisX: {
+        title: "Day",
+        minimum: 0,
+    },
+    axisY:[
+        {
+            title: "Sleep (hrs)",
+            lineColor: "#C24642",
+            tickColor: "#C24642",
+            labelFontColor: "#C24642",
+            titleFontColor: "#C24642",
+            includeZero: true,
+            suffix: "h"
+        },
+        {
+            title: "Exercise (mins)",
+            lineColor: "#369EAD",
+            tickColor: "#369EAD",
+            labelFontColor: "#369EAD",
+            titleFontColor: "#369EAD",
+            includeZero: true,
+            suffix: "m"
+        }
+    ],
+    toolTip: {
+		shared: true
+	},
+	legend: {
+		cursor: "pointer",
+	},
     type: 'line',
-    data: {
-        labels: sD.x,
-        datasets: [{
-            data: sD.y,
-        }],
-    }
+    data: [
+        {
+            type: "line",
+            name: "Sleep",
+            color: "#C24642",
+            axisYIndex: 0,
+            showInLegend: true,
+            dataPoints: sleepData,
+        },
+        {
+            type: "line",
+            name: "Exercise",
+            color: "#369EAD",
+            showInLegend: true,
+            axisYIndex: 1,
+            dataPoints: exerciseData,
+        }
+    ]
 }
-
-
-
-new Chart(ctx, cfg);
+// Create & render chart
+let c = new CanvasJS.Chart("mychart", cfg);
+c.render();
